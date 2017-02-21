6a39ef483aab44c02f54109ccf0d341d21a5bf5f
wencongshanghai <364652802@qq.com>
2017-02-21 21:58:51 +0800

    commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports={
+	entry:'./build/app.js',/*入口文件*/
+	output:{/*输出目录*/
+		path:'./js',/*输出目录*/
+		filename:'myapp.js'/*输出文件*/
+	},module:{
+		loaders:[
+			{
+				test:/\.js$/,/*通过入口处理匹配js文件*/
+				loader:'jsx-loader'
+			}
+		]
+	}
+}
